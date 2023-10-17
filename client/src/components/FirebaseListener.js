b31078221f1631ef5bb30b4dc8d048a75fd1aba5
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2023-10-17 15:22:34 -0500

    Create FirebaseListener.js

---
+++ client/src/components/FirebaseListener.js
@@ -0,0 +1,20 @@
+import React, {useEffect} from "react";
+import { ref, onValue } from "firebase/database";
+import { firebaseDatabase } from "../firebase/firebase";
+import { connect } from "react-redux";
+import { update as updateAllCharactersData} from './slices/allCharactersSlice'
+
+const FirebaseListener = ({updateAllCharactersData}) => {
+    useEffect(() => {
+        const usersRef = ref(firebaseDatabase, 'users/');
+        const unsubsribeUsersListener = onValue(usersRef, (snapshot) => {
+            const data = snapshot.val();
+            console.log("FirebaseListener data: ", data);
+            updateAllCharactersData(data);
+        });
+        return unsubsribeUsersListener;
+    }, []);
+    return<></>;
+}
+
+export default connect(null, {updateAllCharactersData})(FirebaseListener);
