38e18c0fb68a418c8a042b6e58ac78e31e132fe5
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2023-10-24 05:54:23 -0500

    Update OtherCharacter.js

--- client/src/components/OtherCharacter.js
+++ client/src/components/OtherCharacter.js
@@ -1,63 +1,73 @@
-import React, {useEffect, useContext} from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect, useContext } from 'react';
+import { connect } from 'react-redux';
 import CanvasConext from './CanvasContext';
-import {CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP} from './characterConstants';
-import {TILE_SIZE} from './mapConstants';
-import {loadCharacter} from './slices/statusSlice';
+import { CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP } from './characterConstants';
+import { TILE_SIZE } from './mapConstants';
+import { loadCharacter } from './slices/statusSlice';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
-import {update as updateAllCharactersData} from './slices/allCharactersSlice';
+import { update as updateAllCharactersData } from './slices/allCharactersSlice';
 import { firebaseDatabase } from '../firebase/firebase';
 import { ref, set, onValue, remove } from "firebase/database";
 
-
-function OtherCharacter({ myCharactersData, loadCharacter, updateAllCharactersData, webrtcSocket }) {
+const myInitData = {
+    name: "name",
+    id: "key",
+    position: { x: 0, y: 0 },
+    characterClass: "characterClass",
+};
+function OtherCharacter({ otherCharactersData, loadCharacter, key, name, x, y, characterClass }) {
     const context = useContext(CanvasConext);
     useEffect(() => {
-        const myInitData = {
-            ...MY_CHARACTER_INIT_CONFIG,
-            socketId: webrtcSocket.id,
+        console.log("OtherCharacter: Entered");
+        myInitData = {
+            name: name,
+            id: key,
+            position: { x: x, y: y },
+            characterClass: characterClass,
         };
-        set(ref(firebaseDatabase, 'users/' + MY_CHARACTER_INIT_CONFIG.id), myInitData);
-        return () => {
-            var myCharacterRef = ref(firebaseDatabase, 'users/' + MY_CHARACTER_INIT_CONFIG.id);
-            remove(myCharacterRef)
-                .then(function(){
-                    console.log("OtherCharacter remove succeeded.");
-                })
-                .catch(function(error){
-                    console.log("OtherCharacter remove failed: " + error.message);
-                });
-        }
-    }, [webrtcSocket]);
+
+        return;
+
+    }, [key, name, x, y, characterClass ]);
 
     useEffect(() => {
-        if (context == null || myCharactersData == null) {
+        if (context == null || otherCharactersData == null) {
             return;
         }
-        const characterImg = document.querySelector(`#character-sprite-img-${MY_CHARACTER_INIT_CONFIG.characterClass}`);
-        const { sx, sy } = CHARACTER_CLASSES_MAP[MY_CHARACTER_INIT_CONFIG.characterClass].icon;
+        const characterImg = document.querySelector(`#character-sprite-img-${myInitData.characterClass}`);
+        const { sx, sy } = CHARACTER_CLASSES_MAP[myInitData.characterClass].icon;
         context.canvas.drawImage(
             characterImg,
             sx,
             sy,
             CHARACTER_IMAGE_SIZE - 5,
             CHARACTER_IMAGE_SIZE - 5,
-            myCharactersData.position.x * TILE_SIZE,
-            myCharactersData.position.y * TILE_SIZE,
+            otherCharactersData.position.x * TILE_SIZE,
+            otherCharactersData.position.y * TILE_SIZE,
             CHARACTER_IMAGE_SIZE,
             CHARACTER_IMAGE_SIZE
         );
         loadCharacter(true);
-    }, [context, myCharactersData?.position.x, myCharactersData?.position.y, loadCharacter]);
+    }, [context, otherCharactersData?.position.x, otherCharactersData?.position.y, loadCharacter]);
 
     return null;
 }
 
 const mapStateToProps = (state) => {
     // console.log("mapStateToProps: state.allCharacters.users: ", state.allCharacters.users);
-    return {myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id]};
+    // return { otherCharactersData: state.allCharacters.users[myInitData.id] };
+    // const myCharacterData = state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id];
+
+    const otherCharactersData = Object.keys(state.allCharacters.users)
+    .filter(id => id != MY_CHARACTER_INIT_CONFIG.id)
+    .reduce((filteredObj, key) => {
+        filteredObj[key] = otherCharactersData[key];
+        return filteredObj;
+    }, []);
+
+    return { otherCharactersData };
 };
 
-const mapDispatch = {loadCharacter};
+const mapDispatch = { loadCharacter };
 
 export default connect(mapStateToProps, mapDispatch)(OtherCharacter);
