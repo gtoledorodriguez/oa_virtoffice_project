a15323586550b79625e9fc4b412524417e716fa7
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2023-10-24 17:20:48 -0500

    added console log

--- client/src/components/MyCharacter.js
+++ client/src/components/MyCharacter.js
@@ -11,6 +11,7 @@ import { ref, set, onValue, remove } from "firebase/database";
 
 
 function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData, webrtcSocket }) {
+    console.log("MY Character: ", MY_CHARACTER_INIT_CONFIG.id, " Position: ", MY_CHARACTER_INIT_CONFIG.position.x, ", ", MY_CHARACTER_INIT_CONFIG.position.y );
     const context = useContext(CanvasConext);
     useEffect(() => {
         const myInitData = {
